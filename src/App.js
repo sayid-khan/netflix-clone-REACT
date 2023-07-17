97170e51f565beec8214d465e7b93ed07d2e014d
sayid-khan <sayidkhan365@gmail.com>
2023-07-17 12:13:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./App.css";
+import requests from "./requests";
+import Row from "./components/Row";
+import Banner from "./components/Banner";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+};
+
+export default App;
